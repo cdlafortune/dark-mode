2e0923db3311200bd09f94c6b7f75d50dbc18abd
cdsuddreth <cdsuddreth@gmail.com>
2020-06-09 22:00:30 -0400

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from "react";
+import {useLocalStorage} from "./useLocalStorage";
+
+export const useDarkMode = () => {
+
+    const [darkMode, setDarkMode] = useLocalStorage("darkmode");
+
+    useEffect(() => {
+       if (darkMode) {
+           document.querySelector('body').classList.add("dark-mode");
+       } else {
+        document.querySelector('body').classList.remove("dark-mode");
+       }
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode];
+}
